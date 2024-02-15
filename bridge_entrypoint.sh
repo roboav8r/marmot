e0e6558a35f2bb6b3c41726f51a7cd575ce1c3f0
John Duncan <30937261+roboav8r@users.noreply.github.com>
2024-02-15 12:45:57 -0600

    Add bridge Docker files

---
+++ bridge_entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "ROS1 bridge entrypoint"
+
+# Source ROS2 and bridge environments
+source /opt/ros/$ROS1_DISTRO/setup.bash
+source /opt/ros/$ROS2_DISTRO/setup.bash
+cd /bridge_ws
+source install/setup.bash
+
+# Display bridged messages and launch the bridge
+ros2 run ros1_bridge dynamic_bridge --print-pairs
+ros2 run ros1_bridge dynamic_bridge
+
+exec "$@"
