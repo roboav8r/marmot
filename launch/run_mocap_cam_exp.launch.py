923827217e2086e6959b50f69dfe2ef55e211d82
John Duncan <john.a.duncan@utexas.edu>
2024-03-05 13:05:02 -0600

    Add initial mocap cam files

---
+++ launch/run_mocap_cam_exp.launch.py
@@ -0,0 +1,62 @@
+import os
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from tracetools_launch.action import Trace
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # Get configuration from yaml file
+    exp_config = os.path.join(
+        get_package_share_directory('marmot'),
+        'config',
+        'mocap_cam_exp.yaml'
+        )
+    def_config = os.path.join(
+        get_package_share_directory('marmot'),
+        'config',
+        'nusc_baseline.yaml'
+    )
+
+    # Manager node
+    comp_node = Node(
+        package='marmot',
+        executable='mocap_cam_exp_mgr.py',
+        name='mocap_cam_exp_mgr_node',
+        output="screen",
+        parameters=[exp_config]
+    )
+    ld.add_action(comp_node)
+
+    # Detection preprocessing nodes
+    det_l_node = Node(
+        package='marmot',
+        executable='depthai_preproc',
+        name='depthai_l_preproc_node',
+        remappings=[('/nuscenes_detections','/detections' )],
+        parameters=[exp_config]
+    )
+    ld.add_action(det_l_node)
+    det_r_node = Node(
+        package='marmot',
+        executable='depthai_preproc',
+        name='depthai_r_preproc_node',
+        remappings=[('/nuscenes_detections','/detections' )],
+        parameters=[exp_config]
+    )
+    ld.add_action(det_r_node)
+
+
+    # # Tracker node
+    # trk_node = Node(
+    #     package='marmot',
+    #     executable='tbd_node.py',
+    #     name='tbd_tracker_node',
+    #     output='screen',
+    #     remappings=[('/detections','/converted_detections')],
+    #     parameters=[def_config]
+    # )
+    # ld.add_action(trk_node)
+
+    return ld
